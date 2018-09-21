c1b30f74f7571942ed4dd23c8a37d1f8507f4929
zhihua-zheng <greengo@outlook.com>
2018-09-20 17:58:34 -0700

    Rescale 'l/q' in vTKE calculation under stable stratification

--- analyzing/tool_library/get_v_tke.m
+++ analyzing/tool_library/get_v_tke.m
@@ -41,7 +41,6 @@ g = 9.81;
 % divided by (-rho_0) to get thermal expanison coefficient (positive)
 alpha = - model_par.dtr0/model_par.rho_0; 
 
-
 %% Deal with staggered grid
 
 % This interpolation approach may need to be modified in future
@@ -54,13 +53,27 @@ T = repmat(out.time',size(Z,1),1);
 u_stokes = interp2(T,Z,out.u_stokes,Ti,Zi,'linear');
 v_stokes = interp2(T,Z,out.v_stokes,Ti,Zi,'linear');
 
+%% Rescale l/q under stable stratification
+
+L = out.L;
+NN_stable = out.NN; % buoyancy frequency
+NN_stable(NN_stable<0) = NaN;
+N = sqrt(NN_stable);
+N = interp2(T,Z,N,Ti,Zi,'linear');
+
+l_over_q = L./q; % time scale for turbulence
+r_Ozm = 0.53./N; % time scale cooresponding to Ozmidov length scale 
+
+% replace the large values of l/q with r_Ozm
+l_over_q(l_over_q > r_Ozm) = r_Ozm(l_over_q > r_Ozm);
+
 %% Stokes shear
 [~, uStokes_z] = gradient(u_stokes,dt*nsave,out.z);
 [~, vStokes_z] = gradient(v_stokes,dt*nsave,out.z);
 
 %% Computation
 
-w_w = q2*(1-6*A1/B1)/3 + (6*A1*out.L./q).*(alpha*g*theta_w - ...
+w_w = q2*(1-6*A1/B1)/3 + (6*A1*l_over_q).*(alpha*g*theta_w - ...
     u_w.*uStokes_z - v_w.*vStokes_z);
 
 
