eaf1d6ef5d2b9173496b5fedb2a8a98a41ce6aeb
zhihua-zheng <greengo@outlook.com>
2018-09-21 16:23:44 -0700

    Add more plotting methods in function for matrix structure inspection

--- analyzing/tool_library/look_m.m
+++ analyzing/tool_library/look_m.m
@@ -1,4 +1,4 @@
-function look_m(mat)
+function look_m(mat,plot_method)
 
 % test_m
 %==========================================================================
@@ -13,18 +13,36 @@ function look_m(mat)
 % INPUT:
 %
 %  mat - input 2-D matrix
-%  c_sym - variable to specify using symmetric colormap (1) or not (0)
+%  plot_method - scalar to specify which plotting method to use 
+%    contourf - 1
+%      pcolor - 2
+%     imagesc - 3
 %
 % OUTPUT:
 %
-%  contour figure of the matrix
+%  figure displaying the structure of the matrix
 %
 % AUTHOR:
 %  September 17 2018. Zhihua Zheng                       [ zhihua@uw.edu ]
 %
 
 figure('position', [0, 0, 900, 300])
-contourf(mat,'LineStyle','none')
+
+switch plot_method
+    case 1 % contourf
+        contourf(mat,'LineStyle','none')
+    
+    case 2 % pcolor
+        [nr,nc] = size(mat);
+        h = pcolor([mat nan(nr,1); nan(1,nc+1)]); % padding the last row and column 
+        set(h, 'EdgeColor', 'none');
+        
+    case 3 % imagesc
+        imAlpha=ones(size(mat));
+        imAlpha(isnan(mat))=0; % set color of NaNs to background color
+        imagesc(mat,'AlphaData',imAlpha)
+        axis('xy') % flip y axis, imagesc mapps the matrix from lower-left corner
+end
 
 cmocean('balance')
 c_max = max(max(abs(mat)));
